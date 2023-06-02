d99e4ab733abed833f1090cbc14b43efa11231e8
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-06-02 11:51:08 +0500

    instal firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import { initializeApp } from "firebase/app"
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCigry1OjPlDVj9Uo_EGtnb4NP4wHbIYJE",
+  authDomain: "fir-9-c3a32.firebaseapp.com",
+  projectId: "fir-9-c3a32",
+  storageBucket: "fir-9-c3a32.appspot.com",
+  messagingSenderId: "1050015084079",
+  appId: "1:1050015084079:web:b22a691cec20266a9c60da",
+};
 
-console.log("Hello i'm Samandar. This is firebase curse")
+initializeApp(firebaseConfig)
