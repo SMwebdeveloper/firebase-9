17f6cbbd89e0041524242b748df2cdeed25e5071
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-06-02 15:08:20 +0500

    add data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCigry1OjPlDVj9Uo_EGtnb4NP4wHbIYJE",
@@ -9,4 +10,23 @@ const firebaseConfig = {
   appId: "1:1050015084079:web:b22a691cec20266a9c60da",
 };
 
+// init firebase app
 initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = []
+    snapshot.docs.forEach(doc => {
+        books.push({...doc.data(), id:doc.id})
+    })
+    console.log(books)
+  }).catch(err => {
+    console.log(err.message)
+  })
