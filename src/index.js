3cb884cfacdb2cd94ffffa8daeeb97484b18a0ef
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-06-03 19:35:17 +0500

    signing user up

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import {
   serverTimestamp,
   updateDoc,
 } from "firebase/firestore";
-import { getAuth } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCigry1OjPlDVj9Uo_EGtnb4NP4wHbIYJE",
@@ -29,7 +29,7 @@ initializeApp(firebaseConfig);
 
 // init services
 const db = getFirestore();
-const auth = getAuth()
+const auth = getAuth();
 
 // collection ref
 const colRef = collection(db, "books");
@@ -90,4 +90,19 @@ updateForm.addEventListener("submit", (e) => {
   }).then(() => {
     updateForm.reset();
   });
+});
+
+// signing users up
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signupForm.email.value
+  const password = signupForm.password.value
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+        console.log('user created:', cred.user)
+        signupForm.reset()
+    }).catch((err) => console.log(err.message))
 });
